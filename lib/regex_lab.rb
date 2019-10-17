784ddba0c172e51bdbf71e658aee48e992a1e64e
Melissa Christman <melchrstmn@gmail.com>
2019-10-17 23:41:20 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,8 +11,8 @@ text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-if text.match(/^[A-Z].*\.$/) ? false : true
-end
+text.match(/^[A-Z].*\.$/) ? false : true
+
 end
 
 def valid_phone_number?(phone)
