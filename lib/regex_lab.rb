06dd0511575e04570ab1298c4a29d60cf25f7b55
Melissa Christman <melchrstmn@gmail.com>
2019-10-17 23:42:05 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,20 +1,20 @@
 def starts_with_a_vowel?(word)
- word.match(/\A[aeiou]/i) ? true : false
+  word.match(/\A[aeiou]/i) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-text.scan(/un\w+ing/)
+  text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-text.scan(/\b\w{5}\b/)
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-text.match(/^[A-Z].*\.$/) == nil ? false : true
+  text.match(/^[A-Z].*\.$/) == nil ? false : true
 
 end
 
 def valid_phone_number?(phone)
-
+  phone.match(/\b\(?\d{3}\)?\s?-?\d{3}-?\s?\d{4}\b/) ? true : false
 end
